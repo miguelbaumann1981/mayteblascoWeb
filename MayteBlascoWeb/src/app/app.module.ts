7e158c2738031b9487c261f0b3777e1b327dd58a
MiguelMartinezBBVA <miguel.martinez3.contractor@bbva.com>
2022-10-16 11:37:45 +0200

    fuente

--- MayteBlascoWeb/src/app/app.module.ts
+++ MayteBlascoWeb/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSliderModule } from '@angular/material/slider';
-import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +13,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
