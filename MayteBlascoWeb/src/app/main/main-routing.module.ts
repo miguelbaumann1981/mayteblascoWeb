df45bfadf7fa913f866154f35d4c4ae1afdeb27e
MiguelMartinezBBVA <miguel.martinez3.contractor@bbva.com>
2022-10-20 17:40:23 +0200

    landing page

--- MayteBlascoWeb/src/app/main/main-routing.module.ts
+++ MayteBlascoWeb/src/app/main/main-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { AwardsComponent } from './pages/awards/awards.component';
+import { InterviewsComponent } from './pages/interviews/interviews.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {
@@ -14,6 +17,18 @@ const routes: Routes = [
       {
         path: 'aboutMe',
         component: AboutMeComponent
+      },
+      {
+        path: 'awards',
+        component: AwardsComponent
+      },
+      {
+        path: 'interviews',
+        component: InterviewsComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
       },
       {
         path: '**',
